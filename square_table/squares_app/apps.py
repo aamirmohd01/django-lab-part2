88b3750ab776c43653671ecd8533117952ad19e3
aamirmohd01 <110297027+aamirmohd01@users.noreply.github.com>
2024-07-21 23:02:37 +0530

    Delete square_table directory

--- square_table/squares_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SquaresAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'squares_app'
--- square_table/squares_app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('squares/<int:num1>/<int:num2>/', views.square_pairs, name='square_pairs'),
-]
